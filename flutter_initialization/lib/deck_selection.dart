cabd907d6cb023f2cfe053e1c2280ad5aed43f14
aphoticSunshine <kris.small24@GMAIL.com>
2024-03-06 18:41:33 -0500

    Added functionality to the deck selection
    added functionality to deck selection, passes the deck selected to the quiz page

--- flutter_initialization/lib/deck_selection.dart
+++ flutter_initialization/lib/deck_selection.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'databaseHelper.dart' as db;
-import 'deck.dart';
+import 'deck.dart' as customDeck;
 import 'quiz_page.dart';
 
 class DeckSelection extends StatefulWidget {
@@ -9,8 +9,7 @@ class DeckSelection extends StatefulWidget {
 }
 
 class _DeckSelectionState extends State<DeckSelection> {
-  List<Deck> _decks = []; // Store the retrieved decks here
-
+  List<customDeck.Deck> _decks = []; // Store the retrieved decks here
   @override
   void initState() {
     super.initState();
@@ -19,17 +18,39 @@ class _DeckSelectionState extends State<DeckSelection> {
 
   Future<void> _fetchDecks() async {
     List<Map<String, dynamic>> deckMaps = await db.DatabaseHelper.instance.getAllDecks();
-    List<Deck> decks = deckMaps.map((deckMap) => Deck.fromJson(deckMap)).toList();
+    List<customDeck.Deck> decks = deckMaps.map((deckMap) => customDeck.Deck.fromJson(deckMap)).toList();
     setState(() {
       _decks = decks;
     });
   }
 
-  void _selectDeck(Deck deck) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => QuizPage(questions: [], answers: [])),
-    );
+  Future<List<customDeck.Card>> _fetchCardsForDeck(customDeck.Deck deck) async {
+    List<Map<String, dynamic>> cardMaps = await db.DatabaseHelper.instance.getCardsForDeck(deck.id!);
+    return cardMaps.map((cardMap) => customDeck.Card.fromJson(cardMap)).toList();
+  }
+
+  void _selectDeck(customDeck.Deck deck) async {
+    List<customDeck.Card> cards = await _fetchCardsForDeck(deck);
+    if (cards.isNotEmpty) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => QuizPage(deck: deck, cards: cards)),
+      );
+    } else {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('Error'),
+          content: Text('Selected deck has no cards.'),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: Text('OK'),
+            ),
+          ],
+        ),
+      );
+    }
   }
 
   @override
